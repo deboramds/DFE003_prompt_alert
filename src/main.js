1bc5f8823e130ebe35ffcf21230e9ca4497618b1
deboramds <deborah.mart@hotmail.com>
2023-05-10 17:51:47 -0300

    exercicío-2

--- src/main.js
+++ src/main.js
@@ -7,4 +7,19 @@ const dobroNumero = entrarNumero*2
 
 alert("O dobro do número digitado é " + dobroNumero + ".");
 
+//exercício 2
+
+const entrarValorConta = prompt("Digite o valor da sua conta:");
+
+const percentual = 0.10;
+
+const totalPercent = entrarValorConta * percentual;
+
+const totalConta = parseInt(entrarValorConta)+ parseInt(totalPercent);
+
+alert("O valor total da conta com taxa de serviço é " + totalConta);
+
+
+
+
 
