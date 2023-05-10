07fe9bf32306887416299de8806c96d319f59876
deboramds <deborah.mart@hotmail.com>
2023-05-10 17:54:11 -0300

    exercicio-3

--- src/main.js
+++ src/main.js
@@ -17,9 +17,19 @@ const totalPercent = entrarValorConta * percentual;
 
 const totalConta = parseInt(entrarValorConta)+ parseInt(totalPercent);
 
-alert("O valor total da conta com taxa de serviço é " + totalConta);
+alert("O valor total da conta com taxa de serviço é " + totalConta.toFixed(2));
 
 
+//exercício 3
 
+const valorContaPizza = prompt("Digite o valor da sua conta:");
+
+const numPessoas = prompt("Informe o número de pessoas pagantes:");
+
+const valorPessoa = valorContaPizza/numPessoas;
+
+const totalPessoa = parseInt(valorPessoa);
+
+alert("O valor a pagar por pessoa é de  R$ " + totalPessoa.toFixed(2));
 
 
